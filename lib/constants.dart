36f0f0c5728652ff063ee08221af8ff9c3da6887
adish vijayan p k <2001adish@gmail.com>
2024-08-03 13:06:08 +0530

    added to cart function

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,5 @@ import 'package:flutter/material.dart';
 // const kprimaryColor = Color(0xffff660e);
 const kcontentColor = Color(0xFF201c1c);
 const kprimaryColor = Color(0xFF181414);
-const baseurl = "https://c748-103-175-136-243.ngrok-free.app";
+const baseurl = "https://5e31-117-223-87-162.ngrok-free.app";
 // const baseurl = "http://127.0.0.1:4040";
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:ecommerce_mobile_app/Provider/add_to_cart_provider.dart';
 import 'package:ecommerce_mobile_app/Provider/favorite_provider.dart';
-import 'package:ecommerce_mobile_app/screens/nav_bar_screen.dart';
+import 'package:ecommerce_mobile_app/screens/Auth/login.dart';
 import 'package:ecommerce_mobile_app/view_model/auth_view_model.dart';
+import 'package:ecommerce_mobile_app/view_model/cart_view_model.dart';
 import 'package:ecommerce_mobile_app/view_model/product_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -33,13 +34,16 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider(
             create: (_) => AuthViewModel(),
           ),
+          ChangeNotifierProvider(
+            create: (_) => CartViewModel(),
+          ),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             textTheme: GoogleFonts.mulishTextTheme(),
           ),
-          home: BottomNavBar(),
+          home: Login(),
         ),
       );
 }
